cc9e6b2b368ebf1c9104705e8ab98e5ee271c906
Sushant Tiwari <sushanttiwari2005@gmail.com>
2026-02-18 17:06:11 +0530

    Initial Commit

---
+++ Nurtur/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Nurtur
+//
+//  Created by sushant tiwari on 18/02/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Nurtur/NurturApp.swift
@@ -0,0 +1,17 @@
+//
+//  NurturApp.swift
+//  Nurtur
+//
+//  Created by sushant tiwari on 18/02/26.
+//
+
+import SwiftUI
+
+@main
+struct NurturApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
