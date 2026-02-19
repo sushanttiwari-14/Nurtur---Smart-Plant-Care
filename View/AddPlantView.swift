c43dfd5aa3c524c6fcd3e18a6e8f78413e5c26f9
Sushant Tiwari <sushanttiwari2005@gmail.com>
2026-02-20 03:12:49 +0530

    Implement local persistence, plant notifications, swipe-to-delete, and adaptive light/dark theming improvements.

---
+++ View/AddPlantView.swift
@@ -0,0 +1,18 @@
+//
+//  AddPlantView.swift
+//  Nurtur
+//
+//  Created by sushant tiwari on 19/02/26.
+//
+
+import SwiftUI
+
+struct AddPlantView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddPlantView()
+}
