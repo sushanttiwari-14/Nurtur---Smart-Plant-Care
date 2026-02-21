fdab93200703808f7c794ee4750e9e4ab834a0b9
Sushant Tiwari <sushanttiwari2005@gmail.com>
2026-02-22 03:32:18 +0530

    feat: implement search with section filtering and enhance Add Plant UX
    - Added custom search bar below header in HomeView
    - Connected searchText state to dynamic filtering logic
    - Refactored ViewModel to filter plants before sectioning
    - Updated Overdue, Due Today, and Upcoming sections to react to search input
    - Added Cancel button to AddPlantView for better dismissal control
    - Maintained swipe-to-dismiss support for sheet

---
+++ View/EditPlantView.swift
@@ -0,0 +1,18 @@
+//
+//  EditPlantView.swift
+//  Nurtur
+//
+//  Created by sushant tiwari on 21/02/26.
+//
+
+import SwiftUI
+
+struct EditPlantView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditPlantView()
+}
---
+++ View/PlantDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  PlantDetailView.swift
+//  Nurtur
+//
+//  Created by sushant tiwari on 21/02/26.
+//
+
+import SwiftUI
+
+struct PlantDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PlantDetailView()
+}
